b502367f9a69d655c7f4df5503bb4ad0548a5162
Alfonso Avila <alfonsoavila458@gmail.com>
2018-05-15 12:35:41 -0500

    layerMun

--- layers/layers.js
+++ layers/layers.js
@@ -108,29 +108,40 @@ jsonSource_Regiones.addFeatures(features_Regiones);var lyr_Regiones = new ol.lay
                 source:jsonSource_Regiones, 
                 style: style_Regiones,
                 title: "Regiones"
+            });var format_Municipios = new ol.format.GeoJSON();
+var features_Municipios = format_Municipios.readFeatures(geojson_Municipios, 
+            {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
+var jsonSource_Municipios = new ol.source.Vector();
+jsonSource_Municipios.addFeatures(features_Municipios);var lyr_Municipios = new ol.layer.Vector({
+                source:jsonSource_Municipios, 
+                style: style_Municipios,
+                title: "Municipios"
             });
 
 var group_group1 = new ol.layer.Group({
                                 layers: [lyr_CEDIS_5km,lyr_CEDIS_10km,lyr_CEDIS_15km,lyr_CEDIS_20km],
                                 title: "Mapas de Calor"});
 			
-lyr_CEDIS_5km.setVisible(false);lyr_CEDIS_10km.setVisible(false);lyr_CEDIS_15km.setVisible(false);lyr_CEDIS_20km.setVisible(false);lyr_CEDIS.setVisible(false);lyr_AGEBS.setVisible(false);lyr_LocalidadesRurales.setVisible(false);lyr_LocalidadesUrbanas.setVisible(false);lyr_Regiones.setVisible(true);
-var layersList = [baseLayer,group_group1,lyr_CEDIS,lyr_AGEBS,lyr_LocalidadesRurales,lyr_LocalidadesUrbanas,lyr_Regiones];
+lyr_Municipios.setVisible(true);lyr_CEDIS_5km.setVisible(false);lyr_CEDIS_10km.setVisible(false);lyr_CEDIS_15km.setVisible(false);lyr_CEDIS_20km.setVisible(false);lyr_CEDIS.setVisible(false);lyr_AGEBS.setVisible(false);lyr_LocalidadesRurales.setVisible(false);lyr_LocalidadesUrbanas.setVisible(false);lyr_Regiones.setVisible(true);
+var layersList = [baseLayer,group_group1,lyr_Municipios,lyr_CEDIS,lyr_AGEBS,lyr_LocalidadesRurales,lyr_LocalidadesUrbanas,lyr_Regiones];
 lyr_CEDIS.set('fieldAliases', {'GID': 'GID', 'REGION': 'REGION', 'CVE_MUNICIPIO': 'CVE_MUNICIPIO', 'CVE_CEDIS': 'CVE_CEDIS', 'MUNICIPIO': 'MUNICIPIO', 'CEDIS': 'CEDIS', 'DOMICILIO': 'DOMICILIO', 'TELEFONO': 'TELEFONO', 'LATITUD': 'LATITUD', 'LONGITUD': 'LONGITUD', });
 lyr_AGEBS.set('fieldAliases', {'CVE_ENT': 'CVE_ENT', 'CVE_LOC': 'CVE_LOC', 'CVE_MUN': 'CVE_MUN', 'CVE_AGEB': 'CVE_AGEB', });
 lyr_LocalidadesRurales.set('fieldAliases', {'CVE_ENT': 'CVE_ENT', 'CVE_MUN': 'CVE_MUN', 'CVE_LOC': 'CVE_LOC', 'NOM_LOC': 'NOM_LOC', 'CLAVE_LOC': 'CLAVE_LOC', });
 lyr_LocalidadesUrbanas.set('fieldAliases', {'CVE_ENT': 'CVE_ENT', 'CVE_MUN': 'CVE_MUN', 'CVE_LOC': 'CVE_LOC', 'NOMLOC': 'NOMLOC', 'CLAVE_LOC': 'CLAVE_LOC', });
 lyr_Regiones.set('fieldAliases', {'GID': 'GID','CVE_ENT': 'CVE_ENT', 'CVE_MUN': 'CVE_MUN', 'CLAVEMUN': 'CLAVEMUN', 'NOM_MUN': 'NOM_MUN', 'TOTAL_MUN': 'TOTAL_MUN', 'REGION_NOM': 'REGION_NOM', 'REGION': 'REGION', 'TOTAL_REG': 'TOTAL_REG', });
+lyr_Municipios.set('fieldAliases', {'CVE_ENT': 'CVE_ENT', 'NOM_MUN': 'NOM_MUN', 'CVE_MUN': 'CVE_MUN', 'ClaveMun': 'ClaveMun', });
 lyr_CEDIS.set('fieldImages', {'GID': 'TextEdit','REGION': 'TextEdit', 'CVE_MUNICIPIO': 'TextEdit', 'CVE_CEDIS': 'TextEdit',  'MUNICIPIO': 'TextEdit', 'CEDIS': 'TextEdit', 'DOMICILIO': 'TextEdit', 'TELEFONO': 'TextEdit', 'LATITUD': 'TextEdit', 'LONGITUD': 'TextEdit', });
 lyr_AGEBS.set('fieldImages', {'CVE_ENT': 'TextEdit', 'CVE_LOC': 'TextEdit', 'CVE_MUN': 'TextEdit', 'CVE_AGEB': 'TextEdit', });
 lyr_LocalidadesRurales.set('fieldImages', {'CVE_ENT': 'TextEdit', 'CVE_MUN': 'TextEdit', 'CVE_LOC': 'TextEdit', 'NOM_LOC': 'TextEdit', 'CLAVE_LOC': 'TextEdit', });
 lyr_LocalidadesUrbanas.set('fieldImages', {'CVE_ENT': 'TextEdit', 'CVE_MUN': 'TextEdit', 'CVE_LOC': 'TextEdit', 'NOMLOC': 'TextEdit', 'CLAVE_LOC': 'TextEdit', });
 lyr_Regiones.set('fieldImages', {'GID': 'TextEdit','CVE_ENT': 'TextEdit', 'CVE_MUN': 'TextEdit', 'CLAVEMUN': 'TextEdit', 'NOM_MUN': 'TextEdit', 'TOTAL_MUN': 'TextEdit', 'REGION_NOM': 'TextEdit', 'REGION': 'TextEdit', 'TOTAL_REG': 'TextEdit', });
+lyr_Municipios.set('fieldImages', {'CVE_ENT': 'TextEdit', 'NOM_MUN': 'TextEdit', 'CVE_MUN': 'TextEdit', 'ClaveMun': 'TextEdit', });
 lyr_CEDIS.set('fieldLabels', {'GID': 'inline label','REGION': 'inline label','CVE_MUNICIPIO': 'inline label' ,'CVE_CEDIS': 'inline label', 'MUNICIPIO': 'inline label', 'CEDIS': 'inline label', 'DOMICILIO': 'inline label', 'TELEFONO': 'inline label', 'LATITUD': 'inline label', 'LONGITUD': 'inline label', });
 lyr_AGEBS.set('fieldLabels', {'CVE_ENT': 'inline label', 'CVE_LOC': 'inline label', 'CVE_MUN': 'inline label', 'CVE_AGEB': 'inline label', });
 lyr_LocalidadesRurales.set('fieldLabels', {'CVE_ENT': 'inline label', 'CVE_MUN': 'inline label', 'CVE_LOC': 'inline label', 'NOM_LOC': 'inline label', 'CLAVE_LOC': 'inline label', });
 lyr_LocalidadesUrbanas.set('fieldLabels', {'CVE_ENT': 'inline label', 'CVE_MUN': 'inline label', 'CVE_LOC': 'inline label', 'NOMLOC': 'inline label', 'CLAVE_LOC': 'inline label', });
 lyr_Regiones.set('fieldLabels', {'GID': 'inline label','CVE_ENT': 'inline label', 'CVE_MUN': 'inline label', 'CLAVEMUN': 'inline label', 'NOM_MUN': 'inline label', 'TOTAL_MUN': 'no label', 'REGION_NOM': 'inline label', 'REGION': 'inline label', 'TOTAL_REG': 'no label', 'GID': 'inline label', });
+lyr_Municipios.set('fieldLabels', {'CVE_ENT': 'inline label', 'NOM_MUN': 'inline label', 'CVE_MUN': 'inline label', 'ClaveMun': 'inline label', });
 lyr_Regiones.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
